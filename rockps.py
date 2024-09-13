b44aa3ee791d94e5156edad9cb3f628c40a5684c
Lahari1421 <parasalahari14@gmail.com>
2024-09-13 21:38:21 +0530

    Add files via upload

---
+++ rockps.py
@@ -0,0 +1,42 @@
+import random
+
+def main():
+    print("Welcome to the Rock-Paper-Scissors!")
+    choices = {
+        "0": "Rock",
+        "1": "Paper",
+        "2": "Scissors"
+    }
+    while True:
+        while True:
+            user_input = input("Enter your choice (0 for Rock, 1 for Paper, 2 for Scissors): ")
+            if user_input in choices:
+                break
+            else:
+                print("Invalid input. Please enter '0', '1', or '2'")
+        user_choice = choices[user_input]
+        computer_input = str(random.randint(0, 2)) 
+        computer_choice = choices[computer_input]
+
+        win_conditions = {
+            "Rock": "Scissors",
+            "Paper": "Rock",
+            "Scissors": "Paper"
+        }
+
+        if win_conditions[user_choice] == computer_choice:
+            print(f"You win! {user_choice} beats {computer_choice}")
+        elif user_choice == computer_choice:
+            print(f"It's a tie! Both chose {user_choice}")
+        else:
+            print(f"Computer wins! {computer_choice} beats {user_choice}")
+
+        play_again = input("Play again? (Y/N): ").lower()
+        if play_again != "y" and play_again != "n":
+            print("Invalid input. Please enter 'Y' or 'N'.")
+        elif play_again != "y":
+            print("Thank you for playing!")
+            break
+
+if __name__ == "__main__":
+    main()
